c7e1dcb55b0963fc55a28859f80be6ee1154f004
cristonal araneda <macair@MacBook-Air-de-Air.local>
2021-07-12 15:23:52 -0400

    implemetacion de slider con swiper

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,18 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-respnse';
+import { Swiper } from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
-  styles: [
-  ]
+  styleUrls: ['./slideshow.components.css']
+
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
 
   constructor() { }
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      loop: true,
 
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+      })
+  }
+   
   ngOnInit(): void {
 
     console.log(this.movies);
